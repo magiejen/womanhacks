bb34589f3d14c6084e59e8881c36b144cff489c7
April Sanchez <aprilsanchez@169-231-120-27.wireless.ucsb.edu>
2020-01-18 17:42:57 -0800

    Uploading UI stuff

---
+++ Bobud/Bobud/View Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Bobud
+//
+//  Created by April Sanchez on 1/18/20.
+//  Copyright © 2020 womxnhacks20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var teaImage
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
